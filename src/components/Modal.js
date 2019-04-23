f2e970c8182f5f63919ea94cf9d5df4c458a8cbe
Sergio <sergio@Sergios-MacBook-Pro.local>
2019-04-23 19:33:18 -0300

    fixed bug with form on enter press

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -11,18 +11,20 @@ const Modal = ({
   <div className="overlay">
     <div className="overlay-content">
       <h2>Add Item</h2>
-      <form className="add-form" action="">
+      <form className="add-form" action="" onSubmit={handleAddItem}>
         <input
           type="text"
           placeholder="Add item"
           value={inputValue}
           onChange={handleInputChange}
+          autoFocus
         />
         <div className="button-row">
-          <button className="btn red" onClick={handleCancel}>
+          <button className="btn red" onClick={handleCancel} type="button">
             Cancel
           </button>
           <button
+            type="submit"
             className="btn blue"
             onClick={handleAddItem}
             disabled={disabledButton || isLoading ? true : false}
